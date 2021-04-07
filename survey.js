235713c02aa9d4517ed574723131ebeacd0065b6
Nelli Butenko <butenkonelly93@gmail.com>
2021-04-07 01:48:23 +0000

    Fixed spaces

--- survey.js
+++ survey.js
@@ -12,9 +12,7 @@ rl.question('What\'s your name ? Nicknames are also acceptable : )', (name) => {
         rl.question('What\'s your favourite thing to eat for that meal ?', (food) => {
           rl.question('Which sport is your absolute favourite ?', (sport) => {
             rl.question('What is your superpower ? In a few words, tell us what you are amazing at!', (superpower) => {
-              console.log(`${name} loves listening to ${listen} while ${activity},
-               devouring ${food} for ${meal}, prefers ${sport} over any other sport, 
-               and is amazing at ${superpower}.`);
+              console.log(`${name} loves listening to ${listen} while ${activity}, devouring ${food} for ${meal}, prefers ${sport} over any other sport, and is amazing at ${superpower}.`);
 
   rl.close();
 
