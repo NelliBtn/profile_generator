83c89d59f96c4fba0b8b1bca390347dde4ac60c1
Nelli Butenko <butenkonelly93@gmail.com>
2021-04-07 01:47:11 +0000

    Fixed the formed generated output description

--- survey.js
+++ survey.js
@@ -5,20 +5,16 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What\'s your name ? Nicknames are also acceptable : )', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-  rl.question('What\'s an activity you like doing ?', (answer) => {
-    console.log(`Thank you for your valuable feedback: ${answer}`);
-    rl.question('What do you listen to while doing that ?', (answer) => {
-      console.log(`Thank you for your valuable feedback: ${answer}`);
-      rl.question('Which meal is your favourite(eg: dinner, brunch, etc.)', (answer) => {
-        console.log(`Thank you for your valuable feedback: ${answer}`);
-        rl.question('What\'s your favourite thing to eat for that meal ?', (answer) => {
-          console.log(`Thank you for your valuable feedback: ${answer}`);
-          rl.question('Which sport is your absolute favourite ?', (answer) => {
-            console.log(`Thank you for your valuable feedback: ${answer}`);
-            rl.question('What is your superpower ? In a few words, tell us what you are amazing at!', (answer) => {
-              console.log(`Thank you for your valuable feedback: ${answer}`);
+rl.question('What\'s your name ? Nicknames are also acceptable : )', (name) => {
+  rl.question('What\'s an activity you like doing ?', (activity) => {
+    rl.question('What do you listen to while doing that ?', (listen) => {
+      rl.question('Which meal is your favourite(eg: dinner, brunch, etc.)', (meal) => {
+        rl.question('What\'s your favourite thing to eat for that meal ?', (food) => {
+          rl.question('Which sport is your absolute favourite ?', (sport) => {
+            rl.question('What is your superpower ? In a few words, tell us what you are amazing at!', (superpower) => {
+              console.log(`${name} loves listening to ${listen} while ${activity},
+               devouring ${food} for ${meal}, prefers ${sport} over any other sport, 
+               and is amazing at ${superpower}.`);
 
   rl.close();
 
